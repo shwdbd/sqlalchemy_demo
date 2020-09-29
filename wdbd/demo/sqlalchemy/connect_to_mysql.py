b53d1784dfa193d35d174597620338c89fecc014
shwdbd <shwangjj@163.com>
2020-09-29 10:45:50 +0800

    建立项目框架

---
+++ wdbd/demo/sqlalchemy/connect_to_mysql.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+# -*- encoding: utf-8 -*-
+'''
+@File    :   connect_to_mysql.py
+@Time    :   2020/09/29 10:40:12
+@Author  :   Jeffrey Wang
+@Version :   1.0
+@Contact :   shwangjj@163.com
+@Desc    :   Mysql数据库（阿里云在线库）连接
+'''
