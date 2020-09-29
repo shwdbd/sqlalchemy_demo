3e3b1b012fe02f60f3b0946a4a3c3531c1c37d2e
shwdbd <shwangjj@163.com>
2020-09-29 14:53:05 +0800

    数据库连接与持久化（新建数据库结构）

--- wdbd/demo/sqlalchemy/connect_to_mysql.py
+++ wdbd/demo/sqlalchemy/connect_to_mysql.py
@@ -8,3 +8,54 @@
 @Contact :   shwangjj@163.com
 @Desc    :   Mysql数据库（阿里云在线库）连接
 '''
+from sqlalchemy import create_engine
+
+
+# 数据库连接参数
+server_url = "rm-8vbqc3l9ultgf5u37qo.mysql.zhangbei.rds.aliyuncs.com"
+user_id = "fdata"
+user_password = "fd_61875707"
+server_port = "3306"
+db_name = "sqlalchemy_demo"
+engine_echo = False
+engine_max_overflow = 0     # 超过连接池大小外最多创建的连接
+engine_pool_size = 5        # 连接池大小
+engine_pool_timeout = 30    # 池中没有线程最多等待的时间，否则报错
+engine_pool_recycle = -1    # 多久之后对线程池中的线程进行一次连接的回收（重置）
+
+
+# 取得数据库连接对象
+def get_engine():
+    try:
+        # 数据库连接字符串
+        conn_str = 'mysql+mysqlconnector://{user_id}:{user_password}@{server_url}:{server_port}/{db_name}'.format(
+            server_url=server_url,
+            user_id=user_id,
+            user_password=user_password,
+            server_port=server_port,
+            db_name=db_name)
+
+        engine = create_engine(conn_str, echo=engine_echo,
+                               max_overflow=engine_max_overflow,
+                               pool_size=engine_pool_size,
+                               pool_timeout=engine_pool_timeout,
+                               pool_recycle=engine_pool_recycle
+                               )
+        return engine
+    except Exception:
+        print("连接数据库（建立引擎）失败！")
+        return None
+
+
+def get_connection():
+    """取得数据库连接对象
+
+    Returns:
+        [type]: [description]
+    """
+    return get_engine().connect()
+
+
+if __name__ == "__main__":
+    conn = get_connection()
+    print(conn)
---
+++ wdbd/demo/sqlalchemy/db_persistence.py
@@ -0,0 +1,64 @@
+#!/usr/bin/env python
+# -*- encoding: utf-8 -*-
+'''
+@File    :   db_persistence.py
+@Time    :   2020/09/29 11:26:27
+@Author  :   Jeffrey Wang
+@Version :   1.0
+@Contact :   shwangjj@163.com
+@Desc    :   数据库表持久化示例
+
+构建表结构，并在数据库中create表
+
+'''
+from datetime import datetime
+from sqlalchemy import (MetaData, Table, Column, Integer, Boolean,
+                        Numeric, String, DateTime, ForeignKey)
+import wdbd.demo.sqlalchemy.connect_to_mysql as conn
+
+
+# 元数据
+metadata = MetaData()
+
+# 表定义
+cookies = Table('cookies', metadata,
+                Column('cookie_id', Integer(), primary_key=True),
+                Column('cookie_name', String(50), index=True),
+                Column('cookie_recipe_url', String(255)),
+                Column('cookie_sku', String(55)),
+                Column('quantity', Integer()),
+                Column('unit_cost', Numeric(12, 2)))
+
+users = Table('users', metadata,
+              Column('user_id', Integer(), primary_key=True),
+              Column('customer_number', Integer(), autoincrement=True),
+              Column('username', String(15), nullable=False, unique=True),
+              Column('email_address', String(255), nullable=False),
+              Column('phone', String(20), nullable=False),
+              Column('password', String(25), nullable=False),
+              Column('created_on', DateTime(), default=datetime.now),
+              Column('updated_on', DateTime(), default=datetime.now,
+                     onupdate=datetime.now))
+
+orders = Table('orders', metadata,
+               Column('order_id', Integer(), primary_key=True),
+               Column('user_id', ForeignKey('users.user_id')),
+               Column('shipped', Boolean(), default=False))
+
+line_items = Table('line_items', metadata, Column('line_items_id', Integer(), primary_key=True),
+                   Column('order_id', ForeignKey('orders.order_id')),
+                   Column('cookie_id', ForeignKey('cookies.cookie_id')),
+                   Column('quantity', Integer()),
+                   Column('extended_cost', Numeric(12, 2)))
+
+# 数据库表持久化
+engine = conn.get_engine()
+
+if not engine:
+    print("数据库连接失败！")
+else:
+    try:
+        metadata.create_all(engine)
+        print("重构数据库表完成！")
+    except Exception as err:
+        print("数据库持久化失败，原因：" + str(err))
